1aa545f8f523940be1c2a1fa33fd54f4e0684f32
tangyi <tangyi3@staff.weibo.com>
2020-10-13 14:45:44 +0800

    完全背包问题

--- dp/518-coinchange2_test.go
+++ dp/518-coinchange2_test.go
@@ -15,21 +15,21 @@ func TestCoinChange2(t *testing.T) {
 		{[]int{10}, 10, 1},
 	}
 	for _, tt := range cases {
-		actual := coinChange2Dp(tt.input1, tt.input2)
+		actual := change(tt.input1, tt.input2)
 		if actual != tt.expected {
-			t.Errorf("coinChange2Dp(%#v, %#v) = %v; expected %v", tt.input1, tt.input2, actual, tt.expected)
+			t.Errorf("change(%#v, %#v) = %v; expected %v", tt.input1, tt.input2, actual, tt.expected)
 		}
 	}
 }
 
 // 动态规划
-func coinChange2Dp(coins []int, total int) int {
+func change(coins []int, amount int) int {
 	dp := make([][]int, len(coins)+1)
 	for i := 0; i <= len(coins); i++ {
-		dp[i] = make([]int, total+1)
+		dp[i] = make([]int, amount+1)
 	}
 	for i := 0; i <= len(coins); i++ {
-		for j := 0; j <= total; j++ {
+		for j := 0; j <= amount; j++ {
 			if j == 0 {
 				dp[i][0] = 1
 			} else if i == 0 {
@@ -43,5 +43,5 @@ func coinChange2Dp(coins []int, total int) int {
 			}
 		}
 	}
-	return dp[len(coins)][total]
+	return dp[len(coins)][amount]
 }
